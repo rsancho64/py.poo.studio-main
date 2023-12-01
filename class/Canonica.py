13c4a7db20c826c639157d08f9f826782bb4c521
rsancho64 <rsancho@iesrioarba.es>
2023-12-01 08:08:30 +0100

    .

--- class/Canonica.py
+++ class/Canonica.py
@@ -1,24 +1,111 @@
+#! /usr/bin/python3
+
+import json
+import copy
+
+
 class Canonic:
-    
+
     def __init__(self):
-        self.value = 11
+        self.val = {}  # {e1: 11}  # None :/
 
     def __eq__(self, otro):
-        if not isinstance(otro,Canonic):
+        if not isinstance(otro, Canonic):
             return False
-        return self.value == otro.value
+        return self.val == otro.val
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def to_json(self):
+        return json.dumps(self, default=lambda self: self.__dict__)  # type??
+
+    # str vs str :
+    # https://www.toppr.com/guides/python-guide/references/methods-and-functions/methods/built-in/repr/python-repr/
+    # (https://www.toppr.com/guides/python-guide/references/methods-and-functions/)
+
+    def __str__(self):
+        """user-friendly version; 
+        pragma: goal : **readable**
+        container’s __str__ **uses** contained objects’ __repr__
+        """
+        return f"{repr(self.val)}"  # ~ ... self.val.__repr__()
+
+    def __repr__(self):
+        """developer-friendly string; 
+        pragma: goal : **unambiguous**
+        """
+        # return f"{type(self)}@{id(self)}::{repr(self.val)}"  ## as repr
+        return f"{type(self)}@{id(self)}::{self.to_json()}"  # as json
+
+    def __voidcopy(self):  # solo un uso. suprimir?
+        return Canonic()
+
+    def __copy__(self):
+        newone = self.__voidcopy()
+        newone.val = copy.deepcopy(self.val)
+        return newone
+
+    def __canonicformat(self, string):
+        return string.lower().strip()
+
+    def put(self, **kv):
+        """to grow.n.update"""
+        for k, v in kv.items():
+            if isinstance(v, str):
+                v = self.__canonicformat(v)
+            self.val[k] = v
+
+    def pop(self, *keys):
+        """to decrease"""
+        for k in keys:
+            self.val.pop(k, None)
+
+    def __lt__(self, other):
+        """ # TODO
+        see: https://stackoverflow.com/questions/7152497/making-a-python-user-defined-class-sortable-hashable
+        the only method to use sortable ?
+        """
+        return True  # TODO
+
 
 if __name__ == "__main__":
 
-    canonico0 = Canonic()
-    canonico1 = Canonic()
-    print(canonico0 == canonico1)
-    # print(canonico0.equals(canonico1)) # True
+    c0 = Canonic()
+    print(c0)
 
-    canonico1.value = 22
-    print(canonico0 == canonico1)
-    # print(canonico0.equals(canonico1)) # False
+    c0.pop("uno", "dos", "tres")  # k's not in data
 
+    c0.put(nombre="ramon")
+    c0.put(edad=25)
+    c0.put(caracter="guason")
+    print(hash(c0), c0)
 
+    c0.pop("caracter")
+    print(hash(c0), c0)
 
+    c0.put(caracter="  guAsON ")
+    print(hash(c0), c0)
 
+    # cc = []
+    # cc.append(Canonic()) # 1st
+    # cc.append(Canonic()) # 2nd
+
+    # # list comprehensions: https://python-intermedio.readthedocs.io/es/latest/comprehensions.html
+    # # list comprehensions to print:
+    # #    https://stackoverflow.com/questions/37084246/printing-using-list-comprehension
+    # print( *(c for c in cc))
+
+    # cc[0].write(k1="v1")
+    # cc[0].write(k2="v2")
+    # print( *(c for c in cc))
+
+    # cc.append(copy.deepcopy(cc[0]))  # 3rd
+    # print( *(c for c in cc))
+
+    # print(cc[0] == cc[2])
+    # print(cc[0] is cc[2])
+
+    # print( *(id(c) for c in cc))
+    # print( *(c.to_json() for c in cc))
+    # print( *(repr(c) for c in cc))
--- class/README.md
+++ class/README.md
@@ -1,18 +1,38 @@
 # contrato para una clase canonica
-must implement:
 
--override-
+- [X] equals() (`__eq__`)
+- [X] toString(`__str__`) and `__repr__`
+- [X] *serializable*
 
-- [X] equals() (__eq__)
-- [ ] toString(__str__)
+*Serialization*: converting an object into a medium that can be saved-into and retrieved-from later:
+e.g., store/transmit the data and then recreate the object when needed using the reverse process -***deserialization***-
+
+in py ..
+
+- [ ] `import pickle` : **not** language agnostic: Cannot unpickle outside py ! **:/**
+- [ ] Hierarchical Data Format 5 (HDF5) (`import h5py`) : cross-platform; works well w Java, C++..
+- [X] **JSON**: `import json` ; use their handy `json.dumps()`
+
+[**read this**](https://pythonprinciples.com/ask/how-do-you-json-serialize-a-class-in-python)
+
+*To make such an object serializable, the easiest solution is to use the* `__dict__` *attribute that all Python objects have. This attribute represents the instance variables as a dictionary. We can use this to write a function to convert an object to JSON*
+
 - [ ] hashCode()
 
+[**read this**](https://www.programiz.com/python-programming/methods/built-in/hash)
 
-- [ ] must be serializable
-- [ ] must be clonable (override clone())
-(is a need for deep versus shallow copies.)
+- [ ] *un/clonable* -a need 4 deep vs shallow copies...
 
-- [ ] must be comparable (with compareTo())
-(must have natural sort order for the class)
+[**read this**](https://www.oreilly.com/library/view/python-cookbook/0596001673/ch05s12.html)
 
-- [ ] MUST have a finalize() 
+*You need to implement the special method* `__copy__` *so your class can cooperate w the `copy.copy` or `copy.deepcopy` If the `__init__` method is slow, you need to bypass it and get an empty object of the class.*
+
+**tip**: If you want instances of a class to be noncopyable, you can define `__copy__` and raise a TypeError there.
+
+- [ ] comparable (`compareTo()`?) ~ have natural sort order (goo: python sortable class ... ¿ just __lt__ ?)
+
+- [ ] MUST have a `finalize()`
+
+*(...) handling of* `__del__()` *methods is notoriously implementation specific, since it depends on internal details of the interpreter’s garbage collector implementation.*
+
+*A more robust alternative can be to define a finalizer which only references the specific functions and objects that it needs, rather than having access to the full state of the object:*
